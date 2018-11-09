35d1d2d1852412d0ebb85529b58985f03580e901
soumoidippan <44861040+soumoidippan@users.noreply.github.com>
2018-11-09 17:32:39 +0530

    Add files via upload

---
+++ Grocery.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShoppingProject
+{
+    public partial class Grocery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if (e.CommandName == "addtocart")
+            {
+                Response.Redirect("Addtocart.aspx?id=" + e.CommandArgument.ToString());
+            }
+        }
+    }
+}
